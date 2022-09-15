05d1809deb60d86ddb20525c643ce7d6ffefbbcc
Ktabibian <Kyle.tabibian@yahoo.com>
2022-09-14 22:13:51 -0400

    Is this good

---
+++ src/main/java/Lab.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Lab {
+    public static String personCheck(String name){
+        if(name.equals("Alice") || name.equals("Bob")){
+            return ("Hello, " + name);
+        }else{
+            return ("Hello");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name;
+        System.out.println("Please type your name.");
+        name = in.next();
+        System.out.println(personCheck(name));
+    }
+}
